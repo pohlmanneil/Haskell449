7f24b73d9d863effad3e6567d5d6d75d090ef4bd
ErickPower <35816207+ErickPower@users.noreply.github.com>
2018-03-15 17:36:12 -0600

    Added functionality for [mach,task] illegal assign
    Added elemAtTotal, and elemAt in Myelem
    Added removeIllegalMachTask in removeIllegal
    Changed name of removeIllegal function to removeIllegalNeighbour
    the functions accept mach in range of 1-8, same with task 1-8

--- Myelem.hs
+++ Myelem.hs
@@ -1,6 +1,8 @@
 module Myelem 
 ( elemTotal 
 , elem2 
+, elemAt  
+, elemAtTotal 
 ) where 
 
 import Data.List
@@ -12,5 +14,24 @@ elem2 :: (Eq a) => [a] -> [a] -> Bool
 elem2 pair assign = (elem x0 assign) && (elem x1 assign) && ((ind x1 assign) - (ind x0 assign)==1)
   where x0 = pair!!0
         x1 = pair!!1
+        ind x y = case elemIndex x y of
+          Just n -> n
+          
+
+elemAtTotal :: (Eq a, Integral a) => [a] -> [[a]] -> [Bool]
+elemAtTotal pair assigns = map (elemAt pair) assigns
+
+elemAt :: (Eq a, Integral a) => [a] -> [a] -> Bool
+elemAt pair assign = ((ind task assign) == mach)
+  where task = (pair!!1)
+        mach = case (pair!!0) of
+          1 -> 0
+          2 -> 1
+          3 -> 2
+          4 -> 3
+          5 -> 4
+          6 -> 5
+          7 -> 6
+          8 -> 7
         ind x y = case elemIndex x y of
           Just n -> n
--- removeIllegal.hs
+++ removeIllegal.hs
@@ -1,7 +1,10 @@
 import Myelem
 
-removeIllegal :: (Eq a) => [a] -> [[a]] -> [[a]]
-removeIllegal pair assigns = [ fst x | x <- result, not (snd x)]
+removeIllegalNeighbour :: (Eq a) => [a] -> [[a]] -> [[a]]
+removeIllegalNeighbour pair assigns = [ fst x | x <- result, not (snd x)]
   where result = zip assigns (elemTotal pair assigns)
   
+removeIllegalMachTask :: (Eq a, Integral a) => [a] -> [[a]] -> [[a]]
+removeIllegalMachTask pair assigns = [ fst x | x <- result, not (snd x)]
+  where result = zip assigns (elemAtTotal pair assigns)
     
