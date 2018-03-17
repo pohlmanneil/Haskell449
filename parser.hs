15d4517d7893069b5520bcbc025a42149ea1c5f3
RileyMatchett <35790848+RileyMatchett@users.noreply.github.com>
2018-03-17 16:03:53 -0600

    Add files via upload
    added compareMe and arg input

--- parser.hs
+++ parser.hs
@@ -1,4 +1,8 @@
 import Text.ParserCombinators.Parsec
+import System.IO
+import Control.Monad
+import System.Environment
+import Data.List
 
 data Text = TextChunk String deriving (Show)
 
@@ -20,8 +24,19 @@ fileParser = do
 parseS :: String -> Either ParseError [Text]
 parseS input = parse fileParser "" input
 
+compareMe c f 0 = return()
+compareMe c f s = do
+    let p = c !! s
+    let y = stripPrefix (p) (f)
+    putStrLn (p)
+    if y == (Just "") then putStrLn ("match found")
+    else compareMe (c) (f) (s-1)
+
 main = do
+    args <- getArgs
+    contents <- readFile (head args)
     let dataString = "Name: minimalisticexampleforced partial assignment:forbidden machine:too-near tasks:machine penalties: 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1too-near penalities "
     print dataString
 
     parseS dataString
+    --writeFile (last args) (contents)
